505394e0cb086c677252677525ccda7aadbd030d
dinukashehan99 <shehan.shyaminda@gmail.com>
2022-03-29 11:46:46 +0530

    development

--- StreachSlider/ContentView.swift
+++ StreachSlider/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State var imageArray: [String] = ["img1","img2","img3"]
+    @State var stringArray: [String] = ["Tuutor Information","Overseas Accomodation","Flight Booking Information", "Student Visa"]
     private let timer = Timer.publish(every: 3, on: .main, in: .common).autoconnect()
     @State private var currentIndex = 0
     @State var time = Timer.publish(every: 0.1, on: .current, in: .tracking).autoconnect()
@@ -19,51 +20,80 @@ struct ContentView: View {
             ScrollView(.vertical, showsIndicators: false, content: {
                 GeometryReader { geo in
                     VStack{
-                        VStack{
-                            TabView(selection: $currentIndex){
-                                ForEach(0..<imageArray.count,id:\.self){ index in
-                                    Image(imageArray[index])
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: UIScreen.main.bounds.width, height: 250)
-                                        .clipped()
-                                }
-                            }
-                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                            .onReceive(timer, perform: { _ in
-                                withAnimation(.spring()){
-                                    currentIndex = currentIndex < imageArray.count ? currentIndex + 1 : 0
-                                }
-                            })
-                                .frame(height: geo.frame(in: .global).minY > 0 ? UIScreen.main.bounds.height / 2.2 + geo.frame(in: .global).minY :  UIScreen.main.bounds.height / 2.2)
-                                .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
-                                .onReceive(time, perform: { _ in
-                                    
-                                    let y = geo.frame(in: .global).minY
-                                    
-                                    if -y > (UIScreen.main.bounds.height / 4) - 50 {
-                                        withAnimation{
-                                            self.show = true
-                                        }
-                                    }
-                                    else{
-                                        withAnimation{
-                                            self.show = false
-                                        }
-                                    }
-                                    
-                                })
-                                .frame(height: UIScreen.main.bounds.height / 3.5)
-                        }
-                        VStack {
-                            HStack {
-                                Text("My Bridge")
-                                Spacer()
+                        TabView(selection: $currentIndex){
+                            ForEach(0..<imageArray.count,id:\.self){ index in
+                                Image(imageArray[index])
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                //                                        .frame(height: UIScreen.main.bounds.height / 3.5)
+                                //                                        .clipped()
                             }
                         }
-                        .padding(.top, 15)
+                        .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                        .offset(y: geo.frame(in: .global).minY > 0 ? -geo.frame(in: .global).minY : 0)
+                        .frame(height: geo.frame(in: .global).minY > 0 ? UIScreen.main.bounds.height / 3 + geo.frame(in: .global).minY :  UIScreen.main.bounds.height / 3)
+                        .onReceive(time, perform: { _ in
+                            
+                            let y = geo.frame(in: .global).minY
+                            
+                            if -y > (UIScreen.main.bounds.height / 3) - 50 {
+                                withAnimation{
+                                    self.show = true
+                                }
+                            }
+                            else{
+                                withAnimation{
+                                    self.show = false
+                                }
+                            }
+                            
+                        })
+                        .onReceive(timer, perform: { _ in
+                            withAnimation(.spring()){
+                                currentIndex = currentIndex < imageArray.count ? currentIndex + 1 : 0
+                            }
+                        })
                     }
                 }
+                VStack {
+                    VStack(spacing: 5) {
+                        HStack{
+                            Text("Bridge with us")
+                                .font(.system(size: 30, weight: .semibold))
+                            Spacer()
+                        }
+                        HStack{
+                            Text("Improve skills & have a bright future")
+                                .font(.system(size: 14))
+                            Spacer()
+                        }
+                    }
+                    .padding(.top, 15)
+                    HStack{
+                        Text("Categories")
+                            .font(.system(size: 24, weight: .regular))
+                        Spacer()
+                    }
+                    .padding(.top, 20)
+                    
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        HStack{
+                            ForEach(0..<10, id: \.self){ index in
+                                Text("stringArray[\(index)]")
+                                    .padding(.vertical, 10)
+                                    .padding(.horizontal, 20)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 20)
+                                            .stroke(Color.black, lineWidth: 2)
+                                    )
+                            }
+                        }
+                        .padding()
+                    }
+                }
+                .padding(.top,  UIScreen.main.bounds.height / 3)
+                .padding(.horizontal, 15)
+                
             })
             if self.show{
                 TopView()
